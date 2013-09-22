ab097c0432228343eb320d48cf83ae2eb2f59e75
Erik Johnson <erikjohnson462@gmail.com>
2013-09-22 18:09:16 -0400

    addition

--- index.php
+++ index.php
@@ -20,13 +20,13 @@
 <div class="hero-unit">
              
                 <h1>
-        <p class="text-center">Project one</p>
+        <p class="text-center"Project one</p>
                 </h1>
               
 <p>My name is Erik Johnson, I was born in North Carolina but have been living in the Boston area for about three years. I was adopted when I was two days old and met my biological family when I was 22 (7 and a half years ago) including both of my families I have five sisters and two brothers.</p>
     <p> The code editor I will be using is sublime text 2, this will be the first time using this editor, I have always used adobe.
     </p> 
-    <p>This last summer I went to Spain. Below is a picture of my brother and a few pictures of Spain </p>
+    <p>This last summer I went to Spain. Below is a picture of my brother and a few pictures of Spain 
 
 
 
@@ -46,26 +46,41 @@
                 </ol>
                 <div class="carousel-inner">
                     <div class="active item">
-                  	<img src="fireworks.jpg"> 200 200
+                  	<img src="fireworks.jpg"> 
                         <div class="carousel-caption">
                         </div>
                     </div>
                     <div class="item">
-                      <img src="building.jpg"> 200 200
+                      <img src="building.jpg">
                         <div class="carousel-caption">
                         </div>
                     </div>
                     
                     <div class="item">
-                      <img src="hollyej.jpg"> <
+                      <img src="hollyej.jpg"> 
                         <div class="carousel-caption">
                         </div>
                     </div>
                     <div class="item">
-                     <img src="marcusej.jpg"> 200 200
+                     <img src="marcusej.jpg"> 
                         <div class="carousel-caption">
                         </div>
                     </div>
+ <a class="carousel-control left" href="#slideshow" data-slide="prev">&lsaquo;</a>
+                <a class="carousel-control right" href="#slideshow" data-slide="next">&rsaquo;</a>
+            </div>
+        
+        <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+        
+
+
+</p>
 </div>
+</div>
+</body>
+   
 
-
+        
+<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+<script src="untitled folder 3/bootstrap/js/bootstrap.min.js"></script>
