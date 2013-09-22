812ce7ab7a9698310e8ed37bc439cae5418932f7
Erik Johnson <erikjohnson462@gmail.com>
2013-09-22 18:39:31 -0400

    another update

--- index.php
+++ index.php
@@ -20,67 +20,30 @@
 <div class="hero-unit">
              
                 <h1>
-        <p class="text-center"Project one</p>
+        <p class="text-center"> Project one </p>
                 </h1>
               
 <p>My name is Erik Johnson, I was born in North Carolina but have been living in the Boston area for about three years. I was adopted when I was two days old and met my biological family when I was 22 (7 and a half years ago) including both of my families I have five sisters and two brothers.</p>
     <p> The code editor I will be using is sublime text 2, this will be the first time using this editor, I have always used adobe.
     </p> 
-    <p>This last summer I went to Spain. Below is a picture of my brother and a few pictures of Spain 
+    <p>This last summer I went to Spain. Below is a picture of my brother and I and a few pictures of Spain 
 
-
-
-<div class="container">
+</div>
 <div class="hero-unit">
-<section id="carousel">
-            
-          
-            <div id="slideshow" class="carousel slide">
-                <ol class="carousel-indicators">
-                    <li data-target="#slideshow" data-slide-to="0" class="active"></li>
-                    <li data-target="#slideshow" data-slide-to="1"></li>
-                    <li data-target="#slideshow" data-slide-to="2"></li>
-                    <li data-target="#slideshow" data-slide-to="3"></li>
-                    
-                    
-                </ol>
-                <div class="carousel-inner">
-                    <div class="active item">
-                  	<img src="fireworks.jpg"> 
-                        <div class="carousel-caption">
-                        </div>
-                    </div>
-                    <div class="item">
-                      <img src="building.jpg">
-                        <div class="carousel-caption">
-                        </div>
-                    </div>
-                    
-                    <div class="item">
-                      <img src="hollyej.jpg"> 
-                        <div class="carousel-caption">
-                        </div>
-                    </div>
-                    <div class="item">
-                     <img src="marcusej.jpg"> 
-                        <div class="carousel-caption">
-                        </div>
-                    </div>
- <a class="carousel-control left" href="#slideshow" data-slide="prev">&lsaquo;</a>
-                <a class="carousel-control right" href="#slideshow" data-slide="next">&rsaquo;</a>
-            </div>
+
+<img src="fireworks.jpg" alt="fire works"height="500" width="500"> 
+<img src="building.jpg" alt="building"height="500" width="500">
+<img src="hollyej.jpg" alt="holly erik"height="500" width="500"> 
+<img src="marcusej.jpg" alt="marcus erik"height="500" width="500"> 
+                        
+  </div>                  
+
         
-        <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-        <script src="bootstrap/js/bootstrap.min.js"></script>
         
 
 
 </p>
-</div>
-</div>
+
 </body>
    
 
-        
-<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-<script src="untitled folder 3/bootstrap/js/bootstrap.min.js"></script>
