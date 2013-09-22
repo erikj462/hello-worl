084a4097e4fa5e25dd40ecf715cb98e694601815
Erik Johnson <erikjohnson462@gmail.com>
2013-09-22 17:51:13 -0400

    is this working

--- index.php
+++ index.php
@@ -1,1 +1,71 @@
-<h1>Hello world!</h1>
+<!doctype html>
+<html>
+<head>
+
+                <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" />
+               <link href="page2.css" rel="stylesheet" type="text/css"> 
+            
+            
+            
+            <meta charset="UTF-8">
+            <title>Project one</title>
+</head>
+
+
+<body>
+ 
+
+
+
+<div class="hero-unit">
+             
+                <h1>
+        <p class="text-center">Project one</p>
+                </h1>
+              
+<p>My name is Erik Johnson, I was born in North Carolina but have been living in the Boston area for about three years. I was adopted when I was two days old and met my biological family when I was 22 (7 and a half years ago) including both of my families I have five sisters and two brothers.</p>
+    <p> The code editor I will be using is sublime text 2, this will be the first time using this editor, I have always used adobe.
+    </p> 
+    <p>This last summer I went to Spain. Below is a picture of my brother and a few pictures of Spain </p>
+
+
+
+<div class="container">
+<div class="hero-unit">
+<section id="carousel">
+            
+          
+            <div id="slideshow" class="carousel slide">
+                <ol class="carousel-indicators">
+                    <li data-target="#slideshow" data-slide-to="0" class="active"></li>
+                    <li data-target="#slideshow" data-slide-to="1"></li>
+                    <li data-target="#slideshow" data-slide-to="2"></li>
+                    <li data-target="#slideshow" data-slide-to="3"></li>
+                    
+                    
+                </ol>
+                <div class="carousel-inner">
+                    <div class="active item">
+                  	<img src="fireworks.jpg"> 200 200
+                        <div class="carousel-caption">
+                        </div>
+                    </div>
+                    <div class="item">
+                      <img src="building.jpg"> 200 200
+                        <div class="carousel-caption">
+                        </div>
+                    </div>
+                    
+                    <div class="item">
+                      <img src="hollyej.jpg"> <
+                        <div class="carousel-caption">
+                        </div>
+                    </div>
+                    <div class="item">
+                     <img src="marcusej.jpg"> 200 200
+                        <div class="carousel-caption">
+                        </div>
+                    </div>
+</div>
+
+
